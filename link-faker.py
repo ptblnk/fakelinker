1cb80d0caf08893c64d1d768125a051253c62d78
ptblank <64058049+ptblnk@users.noreply.github.com>
2024-05-21 19:34:41 +1000

    Made embed optional

--- link-faker.py
+++ link-faker.py
@@ -1,7 +1,13 @@
-url = str(input("Link to send: ")).rstrip()
-embed = str(input("Link to embed: ")).rstrip()
+real_url = str(input("Real link: ")).rstrip()
+fake_url = str(input("Fake link: ")).rstrip()
+response = str(input("Do you want to embed the fake link? (y/n): "))
 
-embed_cut = embed[1:]
+embed = True if response == "y" else False
+
+fake_url_cut = fake_url[1:]
 
 template = "[h](<{}>)[{}](<{}>)                         [⠀]({})"
-print(template.format(url, embed_cut, url, embed))
+if embed == False:
+    print(template[:20].format(real_url, fake_url_cut, real_url, fake_url))
+else:
+    print(template.format(real_url, fake_url_cut, real_url, fake_url))
